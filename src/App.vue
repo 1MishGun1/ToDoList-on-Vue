4682a6997d67cfee26e2373e297c6ab541c9231f
1MishGun1 <mikhail.kudrov@gmail.com>
2024-01-09 22:44:45 +0300

    app component

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <header-list />
+  <content-list />
+</template>
+
+<script>
+import HeaderList from "./components/HeaderList.vue";
+import ContentList from "./components/ContentList.vue";
+
+export default {
+  name: "App",
+  components: {
+    HeaderList,
+    ContentList,
+  },
+};
+</script>
+
+<style></style>
