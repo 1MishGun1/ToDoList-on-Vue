996aabc3eae79b723dbe5d1fb67bed5bb53f249a
1MishGun1 <mikhail.kudrov@gmail.com>
2024-01-09 22:45:08 +0300

    list component

---
+++ src/components/ContentList.vue
@@ -0,0 +1,33 @@
+<template>
+  <main class="main">
+    <div class="container">
+      <h3 class="main-title">count tasts:</h3>
+      <ul class="main-items">
+        <!-- <li class="item"></li> -->
+      </ul>
+    </div>
+  </main>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+h3 {
+  margin: 0;
+  padding: 0;
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+.main-title {
+  margin-top: 50px;
+  text-align: center;
+  text-transform: uppercase;
+}
+
+.container {
+  max-width: calc(100% - 500px);
+  margin: 0 auto;
+}
+</style>
